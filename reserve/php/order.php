74d11af990f03f1a144abbb4e87af4785101fd4f
ramjinational <ramjinational@gmail.com>
2016-04-06 23:51:09 +0800

    Update order.php
    Add function to get environment

--- reserve/php/order.php
+++ reserve/php/order.php
@@ -1,5 +1,23 @@
 <?php
 
+//parse vcap
+if( getenv("VCAP_SERVICES") ) {
+    $json = getenv("VCAP_SERVICES");
+} 
+# No DB credentials
+else {
+    echo "No vcap services available.";
+    return;
+}
+# Decode JSON and gather DB Info
+$services_json = json_decode($json,true);
+$blu = $services_json["sqldb"];
+if (empty($blu)) {
+    echo "No dashDB service instance is bound. Please bind a SQLDB service instance.";
+    return;
+}
+$bludb_config = $services_json["sqldb"][0]["credentials"];
+
 // create DB connect string
 $conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=".
    $bludb_config["db"].
